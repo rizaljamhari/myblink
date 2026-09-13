cb3221df45b3f74e5898fc3b48e337d6c95ad059
rizaljamhari <rizaljamhari@gmail.com>
2026-09-13 10:17:17 +0800

    Load configure passthrough fix

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -2,6 +2,7 @@
 
 import myblink
 from compatibility_fixes import apply_runtime_fixes
+from configure_passthrough_fix import apply_configure_passthrough_fix
 from http_debug_bootstrap import apply_http_debug_tracing
 from rate_limit_body_fix import apply_rate_limit_body_fix
 from scheduling_guards import apply_scheduling_guards
@@ -13,6 +14,7 @@ apply_rate_limit_body_fix()
 apply_scheduling_guards()
 apply_http_debug_tracing()
 apply_web_rate_limit_ui()
+apply_configure_passthrough_fix()
 
 
 if __name__ == "__main__":
