ff01e5a8d7854f843e2f70fc16e6aa69bd85d858
rizaljamhari <rizaljamhari@gmail.com>
2026-09-13 10:08:37 +0800

    Enable Blink rate limit countdown UI

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -5,12 +5,14 @@ from compatibility_fixes import apply_runtime_fixes
 from http_debug_bootstrap import apply_http_debug_tracing
 from rate_limit_body_fix import apply_rate_limit_body_fix
 from scheduling_guards import apply_scheduling_guards
+from web_rate_limit_ui import apply_web_rate_limit_ui
 
 
 apply_runtime_fixes(myblink)
 apply_rate_limit_body_fix()
 apply_scheduling_guards()
 apply_http_debug_tracing()
+apply_web_rate_limit_ui()
 
 
 if __name__ == "__main__":
