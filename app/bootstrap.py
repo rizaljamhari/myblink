0533d34e3bffe522930676bad1ffc98d6dc573f6
rizaljamhari <rizaljamhari@gmail.com>
2026-09-13 10:04:24 +0800

    Use Blink 429 body reset window

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -3,10 +3,12 @@
 import myblink
 from compatibility_fixes import apply_runtime_fixes
 from http_debug_bootstrap import apply_http_debug_tracing
+from rate_limit_body_fix import apply_rate_limit_body_fix
 from scheduling_guards import apply_scheduling_guards
 
 
 apply_runtime_fixes(myblink)
+apply_rate_limit_body_fix()
 apply_scheduling_guards()
 apply_http_debug_tracing()
 
