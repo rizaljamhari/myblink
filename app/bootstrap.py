506f03bb71e5f370d68b4ed2c4f93eeeb950bf7b
rizaljamhari <rizaljamhari@gmail.com>
2026-09-13 09:17:43 +0800

    Apply authentication-aware scheduling guards

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -2,9 +2,11 @@
 
 import myblink
 from compatibility_fixes import apply_runtime_fixes
+from scheduling_guards import apply_scheduling_guards
 
 
 apply_runtime_fixes(myblink)
+apply_scheduling_guards()
 
 
 if __name__ == "__main__":
