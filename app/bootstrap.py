f7f5fc11fad33140ac8dbf4a6270af2c544c9407
rizaljamhari <rizaljamhari@gmail.com>
2026-09-13 09:50:48 +0800

    Enable opt-in Blink HTTP tracing bootstrap

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -2,11 +2,13 @@
 
 import myblink
 from compatibility_fixes import apply_runtime_fixes
+from http_debug_bootstrap import apply_http_debug_tracing
 from scheduling_guards import apply_scheduling_guards
 
 
 apply_runtime_fixes(myblink)
 apply_scheduling_guards()
+apply_http_debug_tracing()
 
 
 if __name__ == "__main__":
