f600195cf39e6a3feff43ba69ec1e1d52486498d
rizaljamhari <rizaljamhari@gmail.com>
2026-09-14 14:40:54 +0800

    Use safe notification snooze UI patch

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -5,7 +5,7 @@ from compatibility_fixes import apply_runtime_fixes
 from configure_passthrough_fix import apply_configure_passthrough_fix
 from http_debug_bootstrap import apply_http_debug_tracing
 from notification_snooze_fix import apply_notification_snooze_fix
-from notification_snooze_ui import apply_notification_snooze_ui
+from notification_snooze_ui_safe import apply_notification_snooze_ui_safe
 from rate_limit_body_fix import apply_rate_limit_body_fix
 from schedule_templates_ui import apply_schedule_templates_ui
 from scheduling_guards import apply_scheduling_guards
@@ -19,7 +19,7 @@ apply_http_debug_tracing()
 apply_notification_snooze_fix()
 apply_web_rate_limit_ui()
 apply_configure_passthrough_fix()
-apply_notification_snooze_ui()
+apply_notification_snooze_ui_safe()
 apply_schedule_templates_ui()
 
 
