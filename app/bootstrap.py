cac76fd731213a672cdece7c14445b7a6ef61b90
rizaljamhari <rizaljamhari@gmail.com>
2026-09-14 15:57:57 +0800

    Enable Blink account diagnostics

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -1,6 +1,7 @@
 """MyBlink bootstrap that applies local compatibility fixes."""
 
 import myblink
+from blink_account_diagnostics import apply_blink_account_diagnostics
 from compatibility_fixes import apply_runtime_fixes
 from configure_passthrough_fix import apply_configure_passthrough_fix
 from http_debug_bootstrap import apply_http_debug_tracing
@@ -21,6 +22,7 @@ apply_web_rate_limit_ui()
 apply_configure_passthrough_fix()
 apply_notification_snooze_ui_safe()
 apply_schedule_templates_ui()
+apply_blink_account_diagnostics()
 
 
 if __name__ == "__main__":
